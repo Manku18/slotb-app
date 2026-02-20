fca42c3986ee93bf925f9e69503f630a2cf24f9c
Manku18 <122945547+Manku18@users.noreply.github.com>
2026-02-20 14:33:37 +0530

    Fix: Add missing babel config and root providers to resolve 500 error

--- App.js
+++ App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import TabNavigator from './src/navigation/TabNavigator';
 import { Colors } from './src/constants/Colors';
-import 'react-native-reanimated';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <NavigationContainer>
-        <TabNavigator />
-      </NavigationContainer>
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <NavigationContainer>
+          <TabNavigator />
+        </NavigationContainer>
+      </View>
+    </SafeAreaProvider>
   );
 }
 
